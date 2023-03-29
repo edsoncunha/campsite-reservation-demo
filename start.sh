f632e617f18376362b1ef4071d9fd4b4fb10b365
Edson Cunha <senhoredson@gmail.com>
2023-03-29 00:33:12 -0300

    fix spelling

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Building Application Jar..."
 ./gradlew clean build -x test
 
 echo "Building web app image..."
-docker build -t campingsite-app:latest .
+docker build -t campsite-app:latest .
 
 echo "Starting containers"
 docker-compose up
